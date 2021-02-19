fb5005f981793033818a3b99230c2fbf1ca55679
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-02-19 17:19:36 +0800

    1.日志文件编写，服务注册中心代码修改

--- eureka/src/main/java/com/yxs/EurekaApplication.java
+++ eureka/src/main/java/com/yxs/EurekaApplication.java
@@ -1,13 +1,24 @@
 package com.yxs;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+	private static final Logger Log= LoggerFactory.getLogger(EurekaApplication.class);
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
-	}
 
+	}*/
+	public static void main(String[] args) {
+		SpringApplication app=new SpringApplication(EurekaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		Log.info("启动成功");
+		Log.info("Eureka地址：\t http://127.0.0.1:{}",env.getProperty("server.port"));
+	}
 }
