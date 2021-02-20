97fb021e12c510d5b26a87e8796c3aebc9c34fc6
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-02-20 17:17:53 +0800

    1.新增gateway子模块，增加gateway依赖，并注册到注册中心

--- gateway/src/main/java/com/yxs/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/yxs/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.yxs.system;
+package com.yxs.gateway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,13 +8,13 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SystemApplication {
-	private static final Logger Log= LoggerFactory.getLogger(SystemApplication.class);
+public class GatewayApplication {
+	private static final Logger Log= LoggerFactory.getLogger(GatewayApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication app=new SpringApplication(SystemApplication.class);
+		SpringApplication app=new SpringApplication(GatewayApplication.class);
 		Environment env = app.run(args).getEnvironment();
 		Log.info("启动成功");
-		Log.info("System地址：\t http://127.0.0.1:{}",env.getProperty("server.port"));
+		Log.info("gateway地址：\t http://127.0.0.1:{}",env.getProperty("server.port"));
 	}
 }
