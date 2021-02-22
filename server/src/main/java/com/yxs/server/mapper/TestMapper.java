405641ecb7f713f806fb8749bdedb853d79bcf57
zyxs <3301068083@qq.com>
2021-02-23 00:20:55 +0800

    1.增加公共server

---
+++ server/src/main/java/com/yxs/server/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.yxs.server.mapper;
+
+import com.yxs.server.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+
+}
---
+++ server/src/main/java/com/yxs/server/service/TestService.java
@@ -0,0 +1,19 @@
+package com.yxs.server.service;
+
+import com.yxs.server.domain.Test;
+import com.yxs.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
