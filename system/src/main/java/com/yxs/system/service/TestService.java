2ac41f495336f661c0f418dbe2f18326f6a11f65
zyxs <3301068083@qq.com>
2021-02-22 21:58:27 +0800

    1.数据里驱动问题修改

---
+++ system/src/main/java/com/yxs/system/service/TestService.java
@@ -0,0 +1,19 @@
+package com.yxs.system.service;
+
+import com.yxs.system.domain.Test;
+import com.yxs.system.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
