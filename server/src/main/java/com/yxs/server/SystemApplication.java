c1b2a951f6c3b1da9a658916fe4bcdda0ad7bf09
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-02-23 09:07:27 +0800

    1.server初始化一下

--- server/src/main/java/com/yxs/server/SystemApplication.java
+++ server/src/main/java/com/yxs/server/SystemApplication.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableEurekaClient
 @MapperScan("com.yxs.system.mapper")
 public class SystemApplication {
 	private static final Logger Log= LoggerFactory.getLogger(SystemApplication.class);
