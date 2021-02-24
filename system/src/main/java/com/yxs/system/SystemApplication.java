a8d09c03ec9d8054e70e6db50e086e2f595703ad
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-02-24 12:25:35 +0800

    1.system继承server模块完成，启动成功！

--- system/src/main/java/com/yxs/system/SystemApplication.java
+++ system/src/main/java/com/yxs/system/SystemApplication.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.yxs")
 @MapperScan("com.yxs.server.mapper")
 public class SystemApplication {
 	private static final Logger Log= LoggerFactory.getLogger(SystemApplication.class);
