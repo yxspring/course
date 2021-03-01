5e8e1cad0acf734ca19967581f5f92d18d0d1ebf
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-03-01 19:57:58 +0800

    1.新增business业务模块

---
+++ business/src/main/java/com/yxs/business/config/BusinessApplication.java
@@ -0,0 +1,24 @@
+package com.yxs.business.config;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.yxs")
+@MapperScan("com.yxs.server.mapper")
+public class BusinessApplication {
+	private static final Logger Log= LoggerFactory.getLogger(BusinessApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app=new SpringApplication(BusinessApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		Log.info("启动成功");
+		Log.info("Business地址：\t http://127.0.0.1:{}",env.getProperty("server.port"));
+	}
+}
