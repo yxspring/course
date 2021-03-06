1b625b2f87a892b932f0524e47937ef27347030e
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-03-06 15:52:43 +0800

    1.增加分页组件pageination.vue 2.大章管理页面使用分页组件，可自定义初始每页5条，最多显示5个按钮

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,77 +1,91 @@
 <template>
-    <table id="simple-table" class="table  table-bordered table-hover">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>课程id</th>
-            <th>课程名称</th>
-            <th>操作</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="chapter in chapters">
-            <td>{{chapter.id}}</td>
-            <td>{{chapter.courseId}}</td>
-            <td>{{chapter.name}}</td>
-            <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                    <button class="btn btn-xs btn-success">
-                        <i class="ace-icon fa fa-check bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-info">
-                        <i class="ace-icon fa fa-pencil bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-danger">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-warning">
-                        <i class="ace-icon fa fa-flag bigger-120"></i>
-                    </button>
-                </div>
-
-                <div class="hidden-md hidden-lg">
-                    <div class="inline pos-rel">
-                        <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                            <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+    <div>
+        <p>
+            <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+                <i class="ace-icon fa fa-refresh"></i>
+                刷新
+            </button>
+        </p>
+        <pagination ref="pagination" v-bind:list="list"></pagination>
+        <table id="simple-table" class="table  table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>课程id</th>
+                <th>课程名称</th>
+                <th>操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="chapter in chapters">
+                <td>{{chapter.id}}</td>
+                <td>{{chapter.courseId}}</td>
+                <td>{{chapter.name}}</td>
+                <td>
+                    <div class="hidden-sm hidden-xs btn-group">
+                        <button class="btn btn-xs btn-success">
+                            <i class="ace-icon fa fa-check bigger-120"></i>
                         </button>
 
-                        <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                            <li>
-                                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+                        <button class="btn btn-xs btn-info">
+                            <i class="ace-icon fa fa-pencil bigger-120"></i>
+                        </button>
+
+                        <button class="btn btn-xs btn-danger">
+                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                        </button>
+
+                        <button class="btn btn-xs btn-warning">
+                            <i class="ace-icon fa fa-flag bigger-120"></i>
+                        </button>
+                    </div>
+
+                    <div class="hidden-md hidden-lg">
+                        <div class="inline pos-rel">
+                            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                                <li>
+                                    <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
                                           <span class="blue">
                                             <i class="ace-icon fa fa-search-plus bigger-120"></i>
                                           </span>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                                <li>
+                                    <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
                                           <span class="green">
                                             <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
                                           </span>
-                                </a>
-                            </li>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                                <li>
+                                    <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
                                           <span class="red">
                                             <i class="ace-icon fa fa-trash-o bigger-120"></i>
                                           </span>
-                                </a>
-                            </li>
-                        </ul>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
 </template>
 <script>
+    //分页组件
+    import Pagination from '../../components/pagination.vue';
     export default {
+        //应用分页组件
+        components:{Pagination},
         name: "chapter",
         data:function(){
             return {
@@ -80,18 +94,19 @@
         },
         mounted:function () {
             let _this=this;
-            _this.list();
+            _this.list(1);
             // this.$parent.activeSidebar("business-chapter-sidebar");
         },
         methods:{
-            list(){
+            list(page){
                 let _this=this;
                 _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",{
-                    page:1,
-                    pageSize:1
+                    page:page,
+                    pageSize:_this.$refs.pagination.size,
                 }).then((response)=>{
                     console.log("查询大章列表结果："+response);
                     _this.chapters=response.data.list;
+                    _this.$refs.pagination.render(page,response.data.total);
                 })
             }
         }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -6,3 +6,28 @@ create table `chapter` (
     `name` char(50)  comment '名称',
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment ='大章';
+
+
+insert into chapter(id, course_id, name) values ('00000003','00000000','测试大章3');
+insert into chapter(id, course_id, name) values ('00000004','00000000','测试大章4');
+insert into chapter(id, course_id, name) values ('00000005','00000000','测试大章5');
+insert into chapter(id, course_id, name) values ('00000006','00000000','测试大章6');
+insert into chapter(id, course_id, name) values ('00000007','00000000','测试大章7');
+insert into chapter(id, course_id, name) values ('00000008','00000000','测试大章8');
+insert into chapter(id, course_id, name) values ('00000009','00000000','测试大章9');
+insert into chapter(id, course_id, name) values ('00000010','00000000','测试大章10');
+insert into chapter(id, course_id, name) values ('00000011','00000000','测试大章11');
+insert into chapter(id, course_id, name) values ('00000012','00000000','测试大章12');
+insert into chapter(id, course_id, name) values ('00000013','00000000','测试大章13');
+insert into chapter(id, course_id, name) values ('00000014','00000000','测试大章14');
+insert into chapter(id, course_id, name) values ('00000015','00000000','测试大章15');
+insert into chapter(id, course_id, name) values ('00000016','00000000','测试大章16');
+insert into chapter(id, course_id, name) values ('00000017','00000000','测试大章17');
+insert into chapter(id, course_id, name) values ('00000018','00000000','测试大章18');
+insert into chapter(id, course_id, name) values ('00000019','00000000','测试大章19');
+insert into chapter(id, course_id, name) values ('00000020','00000000','测试大章20');
+insert into chapter(id, course_id, name) values ('00000021','00000000','测试大章21');
+insert into chapter(id, course_id, name) values ('00000022','00000000','测试大章22');
+insert into chapter(id, course_id, name) values ('00000023','00000000','测试大章23');
+insert into chapter(id, course_id, name) values ('00000024','00000000','测试大章24');
+
