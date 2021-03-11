5ffc74094352134259a1fa473fb37137c09dad6d
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-03-11 11:01:35 +0800

    1.增加公共组件tool,validator作为校验工具 2.为大章保存增加前端校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -115,6 +115,12 @@
 
             save(page){
                 let _this = this;
+                /*校验数据是否正确*/
+                if (!Validator.require(_this.chapter.name,"名称")||
+                    !Validator.require(_this.chapter.courseId,"课程ID")||
+                    !Validator.length(_this.chapter.courseId,"课程ID",1,8)){
+                    return;
+                }
                 Loading.show();
                 _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/save",_this.chapter).then((response)=>{
                     Loading.hide();
