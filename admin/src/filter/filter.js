9fc8a5cc58da9f53db32b6665ee03fb707a106ea
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-03-15 19:10:02 +0800

    1.enums前端枚举公共js,section引入enums公共枚举 2.增加枚举生成器enumGenerator

--- admin/src/filter/filter.js
+++ admin/src/filter/filter.js
@@ -1,17 +1,17 @@
 /**
  * 数组过滤器 例如 {{CHARGE | optionKV(section.charge)}}
- * @param list 例如[{key:"C"，value:"收费"},{key:"F",value:"免费"}]
+ * @param object 例如{CHARGE:{key:"C",value:"收费"},FREE:{key: "F",value:"免费"}};
  * @param key 例如：C
  * @return {string} 例如：收费
  * */
-let optionKV = (list,key) => {
-    if (!list || !key){
+let optionKV = (object,key) => {
+    if (!object || !key){
         return "";
     }else{
         let result="";
-        for (let i=0;i<list.length;i++){
-            if (key===list[i]["key"]){
-                result = list[i]["value"];
+        for (let enums in object){
+            if (key===object[enums]["key"]){
+                result = object[enums]["value"];
             }
         }
         return result;
