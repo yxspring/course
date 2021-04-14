be7400cc80c79e8b2ee6660128230e31a6ec2e63
ZYXS <3301068083@qq.com>
2021-04-14 23:51:33 +0800

    1、增加file模块，提交照片成功

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -77,7 +77,8 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <input v-model="teacher.image" class="form-control">
+                  <!--<input v-model="teacher.image" class="form-control">-->
+                  <input type="file" v-on:change="uploadImage()" id="file-upload-input">
                 </div>
               </div>
               <div class="form-group">
@@ -176,7 +177,6 @@
         if (1 != 1
           || !Validator.length(_this.teacher.name, "姓名", 1, 50)
           || !Validator.length(_this.teacher.nickname, "昵称", 1, 50)
-          || !Validator.length(_this.teacher.image, "头像", 1, 100)
           || !Validator.length(_this.teacher.position, "职位", 1, 50)
           || !Validator.length(_this.teacher.motto, "座右铭", 1, 50)
           || !Validator.length(_this.teacher.intro, "简介", 1, 500)
@@ -214,6 +214,18 @@
             }
           })
         });
+      },
+      /*上传图片*/
+      uploadImage(){
+        let _this=this;
+        let formData=new window.FormData();
+        //key:"file"必须和后端controller参数名一致
+        formData.append('file',document.querySelector('#file-upload-input').files[0]);
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER+'/file/admin/upload',formData).then((response)=>{
+          Loading.hide();
+          let resp=response.data;
+        })
       }
     }
   }
---
+++ file/src/main/java/com/yxs/file/controller/admin/UploadController.java
@@ -0,0 +1,35 @@
+package com.yxs.file.controller.admin;
+
+import com.yxs.server.util.ResponseDto;
+import com.yxs.server.util.UuidUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RequestMapping("/admin")
+@RestController
+public class UploadController {
+    private static final Logger log= LoggerFactory.getLogger(UploadController.class);
+
+    @RequestMapping("/upload")
+    public ResponseDto upload(@RequestParam MultipartFile file ) throws IOException {
+        ResponseDto responseDto = new ResponseDto();
+        log.info("文件开始上传:{}",file);
+        log.info(file.getOriginalFilename());
+        log.info(String.valueOf(file.getSize()));
+        //保存文件到本地
+        String fileName=file.getOriginalFilename();
+        String key=UuidUtil.getShortUuid();
+        String fullPath="E:/Person Files/Java/course/doc/oragin/"+key+"-"+fileName;
+        File dest = new File(fullPath);
+        file.transferTo(dest);
+        log.info(dest.getAbsolutePath());
+        return responseDto;
+    }
+}
