928a11af83862c22b7fda535bbdc99d7f3ed6f07
ZYXS <3301068083@qq.com>
2021-04-22 22:01:08 +0800

    讲师头像上传优化

--- file/src/main/java/com/yxs/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/yxs/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.yxs.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,8 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String FILE_PATH;
     @Override
     public  void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/f/**").addResourceLocations("file:F:/personal files/JavaLearning/Spring/master-spring/the project/course/doc/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:"+FILE_PATH);
     }
 }
--- file/src/main/java/com/yxs/file/controller/admin/UploadController.java
+++ file/src/main/java/com/yxs/file/controller/admin/UploadController.java
@@ -4,6 +4,7 @@ import com.yxs.server.util.ResponseDto;
 import com.yxs.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,10 @@ public class UploadController {
     private static final Logger log= LoggerFactory.getLogger(UploadController.class);
     private static final String BUSINESS_NAME = "文件上传";
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+    @Value("${file.domain}")
+    private String FILE_DOMAIN;
     @RequestMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file ) throws IOException {
         ResponseDto responseDto = new ResponseDto();
@@ -27,11 +32,11 @@ public class UploadController {
         //保存文件到本地
         String fileName=file.getOriginalFilename();
         String key=UuidUtil.getShortUuid();
-        String fullPath="F:/personal files/JavaLearning/Spring/master-spring/the project/course/doc/oragin/"+key+"-"+fileName;
+        String fullPath=FILE_PATH+"teacher/"+key+"-"+fileName;
         File dest = new File(fullPath);
         file.transferTo(dest);
         log.info(dest.getAbsolutePath());
-        responseDto.setContent("http://127.0.0.1:9000/file/f/oragin/"+key+"-"+fileName);
+        responseDto.setContent(FILE_DOMAIN+"f/teacher/"+key+"-"+fileName);
         return responseDto;
     }
 }
