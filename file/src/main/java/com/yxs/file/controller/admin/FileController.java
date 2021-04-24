35eddbcbcaf2beba1e3116c92365d492367b23ac
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-04-24 21:00:01 +0800

    1.文件上传功能开发;删除新增，更新，删除等功能

--- file/src/main/java/com/yxs/file/controller/admin/FileController.java
+++ file/src/main/java/com/yxs/file/controller/admin/FileController.java
@@ -1,12 +1,13 @@
 package com.yxs.file.controller.admin;
-import com.yxs.server.dto.FileDto;
 import com.yxs.server.dto.PageDto;
 import com.yxs.server.service.FileService;
 import com.yxs.server.util.ResponseDto;
-import com.yxs.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -28,29 +29,4 @@ public class FileController {
         responseDto.setContent(pageDto);
         return responseDto;
     }
-    /**
-     * 保存文件
-     * */
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody FileDto fileDto){
-        // 保存校验
-            ValidatorUtil.require(fileDto.getPath(), "相对路径");
-            ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-            ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-            ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-        ResponseDto responseDto=new ResponseDto();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-    /**
-     * 删除文件
-     * */
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id){
-        ResponseDto responseDto=new ResponseDto();
-        fileService.delete(id);
-        return responseDto;
-    }
-
 }
