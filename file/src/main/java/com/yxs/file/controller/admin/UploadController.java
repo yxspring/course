20a849246bee01eb2ae70551549059d4afa58f24
ZYXS <37678999+yxspring@users.noreply.github.com>
2021-04-24 20:43:13 +0800

    1.文件上传功能开发;上传文件时保存文件记录

--- file/src/main/java/com/yxs/file/controller/admin/UploadController.java
+++ file/src/main/java/com/yxs/file/controller/admin/UploadController.java
@@ -1,5 +1,7 @@
 package com.yxs.file.controller.admin;
 
+import com.yxs.server.dto.FileDto;
+import com.yxs.server.service.FileService;
 import com.yxs.server.util.ResponseDto;
 import com.yxs.server.util.UuidUtil;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 
@@ -19,6 +22,9 @@ public class UploadController {
     private static final Logger log= LoggerFactory.getLogger(UploadController.class);
     private static final String BUSINESS_NAME = "文件上传";
 
+    @Resource
+    private FileService fileService;
+
     @Value("${file.path}")
     private String FILE_PATH;
     @Value("${file.domain}")
@@ -26,17 +32,26 @@ public class UploadController {
     @RequestMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file ) throws IOException {
         ResponseDto responseDto = new ResponseDto();
+        String key=UuidUtil.getShortUuid();
         log.info("文件开始上传:{}",file);
         log.info(file.getOriginalFilename());
         log.info(String.valueOf(file.getSize()));
         //保存文件到本地
         String fileName=file.getOriginalFilename();
-        String key=UuidUtil.getShortUuid();
-        String fullPath=FILE_PATH+"teacher/"+key+"-"+fileName;
+        String suffix=fileName.substring(fileName.lastIndexOf(".")+1).toLowerCase();
+        String path="teacher/"+key+"."+suffix;
+        String fullPath=FILE_PATH+path;
         File dest = new File(fullPath);
         file.transferTo(dest);
-        log.info(dest.getAbsolutePath());
-        responseDto.setContent(FILE_DOMAIN+"f/teacher/"+key+"-"+fileName);
+        log.info("保存文件上传记录开始：");
+        FileDto fileDto = new FileDto();
+        fileDto.setPath(path);
+        fileDto.setName(fileName);
+        fileDto.setSize(Math.toIntExact(file.getSize()));
+        fileDto.setSuffix(suffix);
+        fileDto.setUse("");
+        fileService.save(fileDto);
+        responseDto.setContent(FILE_DOMAIN+path);
         return responseDto;
     }
 }
